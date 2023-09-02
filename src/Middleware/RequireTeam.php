6950bfd6ea3258bda74fe0bd366d33de4dedaee8
itbm <22393016+itbm@users.noreply.github.com>
2023-09-02 12:20:02 +0100

    Switch team if currentTeam not set

--- src/Middleware/RequireTeam.php
+++ src/Middleware/RequireTeam.php
@@ -15,6 +15,12 @@ class RequireTeam
         if (auth()->user()->allTeams()->count() === 0) {
             return redirect()->route('teams.create');
         }
+
+        if(! auth()->user()->currentTeam) {
+            if (! auth()->user()->switchTeam(auth()->user()->allTeams()[0])) {
+                abort(403);
+            }
+        }
         
         return $next($request);
     }
